41dc17db461f0e0efe0e16b32e5755ae192c9f4d
Manigandan722 <manivedi722@gmail.com>
2023-10-18 16:48:03 +0530

    todo add

---
+++ JS/ex2/js.js
@@ -0,0 +1,11 @@
+let fname = "manigandan";
+let fplayer = "Dhoni";
+let fmovie = "Vasikagara";
+
+function favourite(){
+    console.log("my favourite name" + " "+ fname);
+    console.log("my favourite player" +" "+ fplayer);
+    console.log("my favourite movie" + ' ' +fmovie);
+}
+
+favourite()
---
+++ JS/ex2/js1.js
@@ -0,0 +1,4 @@
+setTimeout(function(){myfunction("i iove Science!!!");},2000);
+function myfunction(value){
+    document.getElementById("demo").innerHTML = value;
+}
